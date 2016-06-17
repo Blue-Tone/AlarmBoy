d4b4dc84abfea2de5a7461bf791e2e64b3497924
onoche win8 <onozuka69@gmail.com>
2016-06-18 01:20:39 +0900

    sh 実装中

---
+++ tool/linux_mac/node-serialport/SerialTest.sh
@@ -0,0 +1,50 @@
+
+D=`dirname $0`
+source $D/../_define.bat
+
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+node $D/sendSerial.js $COM cdefghijt
+TIMEOUT 2
+node $D/sendSerial.js $COM CDEFGHIJT
+TIMEOUT 2
+
+pause
