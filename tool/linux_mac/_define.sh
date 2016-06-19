5733510e4f09e0076a05ed545b7bff569732ea7f
BlueTone <bluetonemaster@gmail.com>
2016-06-20 03:51:49 +0900

    mac OK

--- tool/linux_mac/_define.sh
+++ tool/linux_mac/_define.sh
@@ -4,22 +4,19 @@ echo off
 # Environment dependent Config. ä¬ã´àÀë∂ê›íË
 
 # set NODE_PATH. for call node-serialport
-export NODE_HOME=/home/yukio/.nvm/versions/node/v6.2.2
+#export NODE_HOME=/home/[username]/.nvm/versions/node/v6.2.2
+export NODE_HOME=/usr/local/
 export NODE_PATH=$NODE_HOME/lib/node_modules
 export PATH=$NODE_HOME/bin:$PATH
 
 # set arduino COM port number
-COM=/dev/ttyUSB0
-export COM
+#export COM=/dev/ttyUSB0
+export COM=/dev/tty.wchusbserial1a1230
 
-# set jenkins userID
-USER=apiUser
-export USER
-
-# set jenkins userAPI token
-API_TOKEN=598b61b95ae3ea1951dd48e123c1c490
-export API_TOKEN
-
+# set jenkins userID and API token
+export USER=apiUser
+#API_TOKEN=598b61b95ae3ea1951dd48e123c1c490
+export API_TOKEN=801fb88b704e724b490f57341876a743
 
 # -------------- define command -------------
 THIS_DEFINE_PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
